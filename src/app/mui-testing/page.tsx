c36ef2c4050f600c6b1bf9829a3ef65cde008ec0
BigStrongToad <durecjuraj8@gmail.com>
2024-09-24 13:24:15 +0200

    Neviem nieco uptade

--- src/app/mui-testing/page.tsx
+++ src/app/mui-testing/page.tsx
@@ -13,6 +13,7 @@ export default function ComboBox() {
       options={top100Films}
       sx={{ width: 300 }}
       renderInput={(params) => <TextField {...params} label="Movie" />}
+      
     />
   );
 }
