060df521ba3783828d64a85d398ed54263720f06
BigStrongToad <durecjuraj8@gmail.com>
2024-09-24 11:16:29 +0200

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+// src/app/page.tsx
+
+import Typography from "@mui/material/Typography";
+
+export const metadata = { title: "Virus | SystemERoar" };
+
+export default function Home() {
+  return (
+    <Typography> Domovská hrianka </Typography>
+  );
+}
