a9eaaa1b65c48822b7d19bb3f3717a7974804423
BigStrongToad <durecjuraj8@gmail.com>
2024-09-24 12:37:01 +0200

    product changes

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+//src/components/Navbar.tsx
+
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+export const metadata = { title: "Navigačný bar | SystemERoar" };
+
+export default function Navbar() {
+  return (
+    <Container>
+      <Typography> Navigačný bar </Typography>
+    </Container>
+  );
+}
---
+++ src/sections/HomeView.tsx
@@ -0,0 +1,14 @@
+//src/sections/HomeView.tsx
+
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+export const metadata = { title: "Domáci pohľad | SystemERoar" };
+
+export default function HomeView() {
+  return (
+    <Container>
+      <Typography> Domáci pohľad </Typography>
+    </Container>
+  );
+}
