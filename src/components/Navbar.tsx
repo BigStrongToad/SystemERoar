9c72f6fec13be88b9815ca970509a601dc6c3602
BigStrongToad <durecjuraj8@gmail.com>
2024-10-08 11:25:06 +0200

    Yooo končime skorej lets goo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,40 @@
 //src/components/Navbar.tsx
 
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
+"use client";
 
-export const metadata = { title: "Navigačný bar | SystemERoar" };
+import * as React from 'react';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import FolderIcon from '@mui/icons-material/Folder';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
-export default function Navbar() {
+export default function LabelBottomNavigation() {
+  const [value, setValue] = React.useState('recents');
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
+
   return (
-    <Container>
-      <Typography> Navigačný bar </Typography>
-    </Container>
+    <BottomNavigation sx={{ width: 500 }} value={value} onChange={handleChange}>
+      <BottomNavigationAction
+        label="Recents"
+        value="recents"
+        icon={<RestoreIcon />}
+      />
+      <BottomNavigationAction
+        label="Favorites"
+        value="favorites"
+        icon={<FavoriteIcon />}
+      />
+      <BottomNavigationAction
+        label="Nearby"
+        value="nearby"
+        icon={<LocationOnIcon />}
+      />
+      <BottomNavigationAction label="Folder" value="folder" icon={<FolderIcon />} />
+    </BottomNavigation>
   );
 }
